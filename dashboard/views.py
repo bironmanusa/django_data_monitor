c4fa828dd89475dbb957be9d9b03436b74763359
Mateo Mayorga <103150863+bironmanusa@users.noreply.github.com>
2025-08-18 19:23:36 +0000

    Django Admin (Autorización)

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 import requests
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 @login_required
+@permission_required('dashboard.index_viewer', raise_exception=True)
 def index(request):
     response = requests.get(settings.API_URL)  # URL de la API
     posts = response.json()  # Convertir la respuesta a JSON
